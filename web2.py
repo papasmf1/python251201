ff637c8acae20a669cd5d3e1702ef8501869b71f
papasmf1 <papasmf1@gmail.com>
2025-12-03 15:51:53 +0900

    웹크롤링 연습3

--- web2.py
+++ web2.py
@@ -4,19 +4,26 @@ from bs4 import BeautifulSoup
 #웹서버에 요청 
 import urllib.request
 
-#주소
-url = "https://www.clien.net/service/board/sold"
+#파일로 저장(write text) 
+f = open("clien.txt", "wt", encoding="utf-8")
 
-#페이지 실행 결과를 문자열로 받기 
-page = urllib.request.urlopen(url).read() 
-#검색이 용이한 객체 
-soup = BeautifulSoup(page, 'html.parser')
+for i in range(0,10):
+    #주소
+    url = "https://www.clien.net/service/board/sold?&od=T31&category=0&po=" + str(i)    
+    print(url)    
+    #페이지 실행 결과를 문자열로 받기 
+    page = urllib.request.urlopen(url).read() 
+    #검색이 용이한 객체 
+    soup = BeautifulSoup(page, 'html.parser')
+    lst = soup.find_all("span", attrs={"data-role":"list-title-text"})
+    for tag in lst:
+        title = tag.text.strip() 
+        print(title)
+        f.write(title + "\n")
 
-lst = soup.find_all("span", attrs={"data-role":"list-title-text"})
-for tag in lst:
-    title = tag.text.strip() 
-    print(title)
-    
+#마지막에 파일 닫기 
+f.close()
+
     # <span class="subject_fixed" data-role="list-title-text" title="(끌어올림) 맥북프로 M3pro 14인치">
     # 			(끌어올림) 맥북프로 M3pro 14인치
     # </span>
